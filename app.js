2602501e7dc67661c93a8f0d851654e8634efd38
abhijith <abhijithcs10@gmail.com>
2021-02-12 11:37:12 +0530

    added local user login and authentication

--- app.js
+++ app.js
@@ -2,9 +2,47 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const session =require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+
+
+const app = express();
+
+
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.use(session({
+  secret: 'This is secret key',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 
 mongoose.connect('mongodb://localhost/foodieClub', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useCreateIndex', true);
+
+const userSchema = new mongoose.Schema({
+  email:String,
+  password:String
+});
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("user",userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 const itemSchema = new mongoose.Schema({
     name:String,
     price:Number
@@ -14,10 +52,9 @@ const Items= new mongoose.model("item",itemSchema);
 
 
 
-const app = express();
 
-app.use(express.static("public"));
-app.set("view engine","ejs");
+
+
 
 //home route
 app.get("/",(req,res)=>{
@@ -47,6 +84,25 @@ app.get("/signup",(req,res)=>{
 res.render("signup")
 })
 
+
+
+app.post("/signup",function(req,res){
+  console.log("evocked")
+  User.register({username:req.body.username}, req.body.password, function(err, user) {
+if(err){
+  console.log(err);
+  res.redirect("/signup");
+}else{
+  console.log("signup else")
+  passport.authenticate("local")(req,res,function(){
+      console.log("test");
+      res.redirect("/");
+  })
+}
+
+});
+})
+
 // signup route
 
 
@@ -54,6 +110,25 @@ app.get("/signin",(req,res)=>{
   res.render("signin")
   })
 
+
+app.post("/signin",function(req,res){
+    const user = new User({
+        username:req.body.username,
+        password:req.body.password
+    });
+ 
+    req.login(user,function(err){
+        if(err){
+            console.log(err)
+        }else{
+         passport.authenticate("local")(req,res,function(){
+             console.log("logedin");
+             res.redirect("/");
+         })
+        }
+    })
+ 
+ });
 
 
 app.listen(3000,()=>{
